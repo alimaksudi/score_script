3a2049cc90134aa90d70a16a6d9eb75c017b3c40
just_ali <ali_maksudi@outlook.com>
2023-07-26 15:54:59 +0700

    Add few lines

--- parse_id_card.py
+++ parse_id_card.py
@@ -1,3 +1,41 @@
+province_list = {
+    '11': 'Aceh',
+    '12': 'Sumatera Utara',
+    '13': 'Sumatera Barat',
+    '16': 'Sumatera Selatan',
+    '14': 'Riau',
+    '21': 'Kepulauan Riau',
+    '15': 'Jambi',
+    '17': 'Bengkulu',
+    '18': 'Lampung',
+    '19': 'Bangka Belitung',
+    '31': 'DKI Jakarta',
+    '36': 'Banten',
+    '32': 'Jawa Barat',
+    '33': 'Jawa Tengah',
+    '34': 'DI Yogyakarta',
+    '35': 'Jawa Timur',
+    '51': 'Bali',
+    '61': 'Kalimantan Barat',
+    '62': 'Kalimantan Tengah',
+    '63': 'Kalimantan Selatan',
+    '64': 'Kalimantan Timur',
+    '65': 'Kalimantan Utara',
+    '52': 'Nusa Tenggara Barat',
+    '53': 'Nusa Tenggara Timur',
+    '71': 'Sulawesi Utara',
+    '72': 'Sulawesi Tengah',
+    '73': 'Sulawesi Selatan',
+    '74': 'Sulawesi Tenggara',
+    '75': 'Gorontalo',
+    '76': 'Sulawesi Barat',
+    '81': 'Maluku',
+    '82': 'Maluku Utara',
+    '91': 'Papua',
+    '92': 'Papua Barat'
+}
+
+
 def parse_id_card(x: str):
     """
     Parses an Indonesian ID card number and returns a list of its components.
@@ -8,6 +46,7 @@ def parse_id_card(x: str):
     Returns:
     list: A list containing the following components of the ID card number:
         - The province code (2 characters)
+        - The province name
         - The city code (4 characters)
         - The district code (6 characters)
         - The gender ('MALE' or 'FEMALE')
@@ -18,7 +57,7 @@ def parse_id_card(x: str):
     # implementation of the function
     
     if len(x) != 16:
-        return [None, None, None, None, None]
+        return [None, None, None, None, None, None]
 
     id_province = x[:2]
     id_city = x[:4]
@@ -34,7 +73,11 @@ def parse_id_card(x: str):
     else:
         id_birthyear = int('19' + x[-6:-4])
 
-    return [id_province, id_city, id_district, id_gender, id_birthyear]
+    province_name = province_list.get(id_province)
 
-a = parse_id_card('5202082711880001')
-print(a)
+    return [province_name, id_city, id_district, id_gender, id_birthyear]
+
+if __name__ == '__main__':
+    id_card_number = input("Enter an Indonesian ID card number: ")
+    parsed_id_card = parse_id_card(id_card_number)
+    print(parsed_id_card)
